ee2e644c69fea6f142a9d6f2110dd9d213d4aeae
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-01-06 02:01:35 +0600

    Create run.py

---
+++ run.py
@@ -0,0 +1,17 @@
+# Programme By Mr Beta
+from os import system
+
+while True:
+    try:import replicate;break
+    except:system('pip install replicate')
+
+while True:
+    try:
+        mod = replicate.models.get("tencentarc/gfpgan")
+        version = model.versions.get("9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3")
+        break
+    except:
+        print("Get Auth Token : https://replicate.com/docs/reference/http#authentication ")
+        auth = input("Enter Auth Token : ")
+        system("export REPLICATE_API_TOKEN="+auth)
+
