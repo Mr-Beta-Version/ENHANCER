6889d37764274c057d16ca3bd037d4d0c803f9c9
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-01-06 02:17:33 +0600

    Update run.py

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ while True:
 
 while True:
     try:
-        mod = replicate.models.get("tencentarc/gfpgan")
-        version = model.versions.get("9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3")
+        tools = replicate.models.get("tencentarc/gfpgan")
+        version = tools.versions.get("9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3")
         break
     except:
         print("Get Auth Token : https://replicate.com/docs/reference/http#authentication ")
