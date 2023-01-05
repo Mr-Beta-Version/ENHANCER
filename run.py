0860e513ef5f88f9cd64bb1242abfc7cd1591991
Mr. Beta <94112333+Mr-Beta-Version@users.noreply.github.com>
2023-01-06 02:32:47 +0600

    Update run.py

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ while True:
 try:
     tools = replicate.models.get("tencentarc/gfpgan")
     version = tools.versions.get("9283608cc6b7be6b65a8e44983db012355fde4132009bf99d976b2f0896856a3")
-    break
 except:
     system("bash token.sh");exit()
 
